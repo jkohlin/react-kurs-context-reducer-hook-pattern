d3f3f4608f4f60a2bab6d5c67d036725f1dd8f00
Johan <johan@kohlin.net>
2023-10-05 17:46:07 +0000

    Update Form.jsx

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,22 +1,37 @@
-import Card from './Card'
-import Checkbox from './Checkbox'
-import Input from './Input'
-import useGlobalState from './useGlobalState'
+import Card from "./Card";
+import Checkbox from "./Checkbox";
+import Input from "./Input";
+import useGlobalState from "./useGlobalState";
 
 export default function Form(props) {
-    const { name, email, newsletter } = useGlobalState()
+  const { name, email, newsletter } = useGlobalState();
 
-    /* 
+  /* 
   Genom att flytta ut reducern till en hook, 
   blir komponenten lättare att överskåda
   */
-    return (
-        <>
-            <div>
-                <Input value={name.value} setter={name.setter} type="text" label="Namn" />
-                <Input value={email.value} setter={email.setter} type="email" label="Epost" />
-                <Checkbox value={newsletter} setter={newsletter.setter} label="Nyhetsbrev" />
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div>
+        <Input
+          value={name.value}
+          setter={name.setter}
+          type="text"
+          label="Namn"
+        />
+        <Input
+          value={email.value}
+          setter={email.setter}
+          type="email"
+          label="Epost"
+        />
+        <p>Nån får gärna se vad jag gjort för fel med checkboxen :D </p>
+        <Checkbox
+          value={newsletter}
+          setter={newsletter.setter}
+          label="Nyhetsbrev"
+        />
+      </div>
+    </>
+  );
 }
